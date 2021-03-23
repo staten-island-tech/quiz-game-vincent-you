cb79506ebe819577ac447396a20c5b91f440ccbc
VincentYou <vincent.you23@sitechhs.com>
2021-03-23 09:05:53 -0400

    pog

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-console.log("check");
+const questions = [
+{
+    question:"What's your name?",
+answerArr: [
+    {position: "A",content: "Vincent" },
+    {position: "B",content: "Mike" },
+    {position: "C",content: "Chris" },
+    ],
+    correctAnswer: "B",
+    },
+];
+
+const DOMSelectors = {
+    begin: document.getElementById("begin")
+};
+
+console.log(DOMSelectors.begin);
