dff5860882aa73083884e203b082585ef92e3131
VincentYou <vincent.you23@sitechhs.com>
2021-04-08 22:08:08 -0400

    sgeeeeeeeeeeeeeeeee

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -118,135 +118,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"js/index.js":[function(require,module,exports) {
-var _ref, _ref2;
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var start = document.getElementById("start");
-var quiz = document.getElementById("quiz");
-var qImage = document.getElementById("qImage");
-var question = document.getElementById("question");
-var counter = document.getElementById("counter");
-var timeGauge = document.getElementById("timeGauge");
-var progress = document.getElementById("progress");
-var scoreContainer = document.getElementById("scoreContainer");
-var choiceA = document.getElementById("A");
-var choiceB = document.getElementById("B");
-var choiceC = document.getElementById("C");
-var questions = [(_ref = {
-  question: "Which animal can live the longest",
-  choiceA: "giraffe",
-  choiceB: "Cow"
-}, _defineProperty(_ref, "choiceA", "Pig"), _defineProperty(_ref, "correct", "A"), _ref), (_ref2 = {
-  question: "Which animal can run the fastest",
-  choiceA: "Rhino",
-  choiceB: "Cheeta"
-}, _defineProperty(_ref2, "choiceA", "Kangaroo"), _defineProperty(_ref2, "correct", "B"), _ref2)];
-var lastQuestion = questions.length - 1;
-var runningQuestion = 0;
-var count = 0;
-var questionTime = 10; // 10s
-
-var gaugeWidth = 150; // 150px
-
-var gaugeUnit = gaugeWidth / questionTime;
-var TIMER;
-var score = 0; // render a question
-
-function renderQuestion() {
-  var q = questions[runningQuestion];
-  question.innerHTML = "<p>" + q.question + "</p>";
-  qImg.innerHTML = "<img src=" + q.imgSrc + ">";
-  choiceA.innerHTML = q.choiceA;
-  choiceB.innerHTML = q.choiceB;
-  choiceC.innerHTML = q.choiceC;
-}
-
-start.addEventListener("click", startQuiz); // start quiz
-
-function startQuiz() {
-  start.style.display = "none";
-  renderQuestion();
-  quiz.style.display = "block";
-  renderProgress();
-  renderCounter();
-  TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
-} // render progress
-
-
-function renderProgress() {
-  for (var qIndex = 0; qIndex <= lastQuestion; qIndex++) {
-    progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
-  }
-} // counter render
-
-
-function renderCounter() {
-  if (count <= questionTime) {
-    counter.innerHTML = count;
-    timeGauge.style.width = count * gaugeUnit + "px";
-    count++;
-  } else {
-    count = 0; // change progress color to red
-
-    answerIsWrong();
-
-    if (runningQuestion < lastQuestion) {
-      runningQuestion++;
-      renderQuestion();
-    } else {
-      // end the quiz and show the score
-      clearInterval(TIMER);
-      scoreRender();
-    }
-  }
-} // checkAnwer
-
-
-function checkAnswer(answer) {
-  if (answer == questions[runningQuestion].correct) {
-    // answer is correct
-    score++; // change progress color to green
-
-    answerIsCorrect();
-  } else {
-    // answer is wrong
-    // change progress color to red
-    answerIsWrong();
-  }
-
-  count = 0;
-
-  if (runningQuestion < lastQuestion) {
-    runningQuestion++;
-    renderQuestion();
-  } else {
-    // end the quiz and show the score
-    clearInterval(TIMER);
-    scoreRender();
-  }
-} // answer is correct
-
-
-function answerIsCorrect() {
-  document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
-} // answer is Wrong
-
-
-function answerIsWrong() {
-  document.getElementById(runningQuestion).style.backgroundColor = "#f00";
-} // score render
-
-
-function scoreRender() {
-  scoreDiv.style.display = "block"; // calculate the amount of question percent answered by the user
-
-  var scorePerCent = Math.round(100 * score / questions.length); // choose the image based on the scorePerCent
-
-  var img = scorePerCent >= 80 ? "img/5.png" : scorePerCent >= 60 ? "img/4.png" : scorePerCent >= 40 ? "img/3.png" : scorePerCent >= 20 ? "img/2.png" : "img/1.png";
-  scoreDiv.innerHTML = "<img src=" + img + ">";
-  scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
-}
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -275,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53553" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54733" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
