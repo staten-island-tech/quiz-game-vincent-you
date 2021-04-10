c0af714a740d73fb5375e5b3de554cfaa9055ff0
VincentYou <vincent.you23@sitechhs.com>
2021-04-10 19:32:31 -0400

    idk  what just hapopened

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -233,7 +233,7 @@ incrementScore = function incrementScore(num) {
 
 console.log(incrementScore);
 startGame();
-},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -261,7 +261,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49633" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54002" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -437,5 +437,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
+},{}]},{},["../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
 //# sourceMappingURL=/js.00a46daa.js.map
