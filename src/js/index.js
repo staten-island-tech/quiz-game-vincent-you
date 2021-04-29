6886272ffdec8d12140e7bd62cafa44d5fa35d73
VincentYou <vincent.you23@sitechhs.com>
2021-04-29 12:40:11 -0400

    d

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ const choices = Array.from(document.querySelectorAll(".choice-text"));
 const scoreText = document.querySelector("#score");
 const progressText = document.querySelector('#progressText')
 const progressBarFull = document.querySelector('#progressBarFull')
+import { questions } from "./questions"
 
 let currentQuestion = {}; //put it into an empty object
 let acceptingAnswers = true;
@@ -11,41 +12,6 @@ let score = 0; //score starts at 0
 let questionCounter = 0; //questions start at 0
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "What animal is the fastest?",
-        choice1: "Penguin",
-        choice2: "Panda",
-        choice3: "Cheeta",
-        choice4: "Lion",
-        answer: 3,
-      },
-      {
-        question: "What's the biggest animal in the planet?",
-        choice1: "Elephant",
-        choice2: "Rhinos",
-        choice3: "Blue whale",
-        choice4: "Giraffe",
-        answer: 3,
-      },
-      {
-        question: "What animal has the longest lifespan?",
-        choice1: "Elephant",
-        choice2: "Tortoise",
-        choice3: "Sea Urchin",
-        choice4: "whales",
-        answer: 2,
-      },
-      {
-        question: "What animal is vegetarian?",
-        choice1: "Hippopotamus",
-        choice2: "Bear",
-        choice3: "Wolf",
-        choice4: "Seals",
-        answer: 1,
-      },
-]
-console.log(questions)
 
 const scorePoints = 100
 const maxQuestions = 4
--- src/js/questions.js
+++ src/js/questions.js
@@ -1,37 +1,36 @@
-let questions = [
+const questions = [
   {
-      question: "What name starts with C ?",
-      choices1: 'Chandler' ,
-      choices2: 'Kristina',
-      choices3: 'Melony',
-      choices4: 'Henry',
-      answer: 1,       
+      question: "What animal is the fastest?",
+      choice1: "Penguin",
+      choice2: "Panda",
+      choice3: "Cheeta",
+      choice4: "Lion",
+      answer: 3,
     },
-  {
-      question: "What name starts with B ?",
-      choices1: 'Chandler' ,
-      choices2: 'Kristina',
-      choices3: 'Melony',
-      choices4: 'Henry',
-      answer: 2,       
+    {
+      question: "What's the biggest animal in the planet?",
+      choice1: "Elephant",
+      choice2: "Rhinos",
+      choice3: "Blue whale",
+      choice4: "Giraffe",
+      answer: 3,
     },
-  {
-      question: "What name starts with Z ?",
-      choices1: 'Chandler' ,
-      choices2: 'Kristina',
-      choices3: 'Melony',
-      choices4: 'Henry',
-      answer: 3,       
+    {
+      question: "What animal has the longest lifespan?",
+      choice1: "Elephant",
+      choice2: "Tortoise",
+      choice3: "Sea Urchin",
+      choice4: "whales",
+      answer: 2,
     },
-    
-  {
-      question: "What name starts with G ?",
-      choices1: 'Chandler' ,
-      choices2: 'Kristina',
-      choices3: 'Melony',
-      choices4: 'Henry',
-      answer: 4,       
-    },     
+    {
+      question: "What animal is vegetarian?",
+      choice1: "Hippopotamus",
+      choice2: "Bear",
+      choice3: "Wolf",
+      choice4: "Seals",
+      answer: 1,
+    },
 ]
 
 export {questions};
